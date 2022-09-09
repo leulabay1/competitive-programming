8e3b66fd4d5989365ed498c58f53939bf85e4c52
leulabay1 <108995687+leulabay1@users.noreply.github.com>
2022-09-10 00:29:50 +0300

    Update 969-pancake-sorting.py

--- 969-pancake-sorting/969-pancake-sorting.py
+++ 969-pancake-sorting/969-pancake-sorting.py
@@ -3,11 +3,13 @@ class Solution:
         temp = arr
         result = []
         for i in range(len(arr), 0, -1):
+            #reversing the last unsorted element to the front
             position = temp.index(i)
             temp_front = temp[0:position+1]
             temp_front.reverse()
             temp_back = temp[position+1:len(arr)]
             temp = temp_front + temp_back
+            #now reversing the element back to it's sorted postion
             temp_front = temp[0: i]
             temp_front.reverse()
             temp_back = temp[i:len(arr)]
