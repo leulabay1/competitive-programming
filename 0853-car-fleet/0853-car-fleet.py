9bd8ec48998f3994fc3b77e4c8dc5402b5656606
leulabay1 <108995687+leulabay1@users.noreply.github.com>
2022-10-19 15:34:04 +0300

    Time: 2716 ms (9.64%), Space: 35.5 MB (67.64%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,13 +1,12 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         ans = 0
+        stack = []
         times = [float(target - p) / s for p, s in sorted(zip(position, speed), reverse=True)]
-        maxTime = 0  # The time of the slowest car to reach the target
-
+        
         for time in times:
-        # A car needs more time to reach the target, so it becomes slowest
-            if time > maxTime:
-                maxTime = time
-                ans += 1
+            stack.append(time)
+            if len(stack) >= 2 and stack[-2] >= stack[-1]:
+                stack.pop()
 
-        return ans
+        return len(stack)
