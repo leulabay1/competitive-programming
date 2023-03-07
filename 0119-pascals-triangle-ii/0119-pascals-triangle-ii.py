d4d843a95549322a0fd9d37cecc6d01d1b3fc556
leulabay1 <108995687+leulabay1@users.noreply.github.com>
2023-03-07 18:20:13 +0300

    Time: 40 ms (17.83%), Space: 13.8 MB (50.83%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -6,9 +6,7 @@ class Solution:
             return [1,1]
         else:
             row = self.getRow(rowIndex-1)
-            ans = [0]*(rowIndex+1)
-            ans[0] = 1
-            ans[-1] = 1
+            ans = [1]*(rowIndex+1)
             for i in range(1, len(row)):
                 ans[i] = row[i-1] + row[i]
             return ans
