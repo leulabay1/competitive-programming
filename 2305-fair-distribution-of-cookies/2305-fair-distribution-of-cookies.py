b3c03cdccdb51916d5c45a9ec0baca6dc7f4f3bc
leulabay1 <108995687+leulabay1@users.noreply.github.com>
2023-03-09 18:55:47 +0300

    Time: 1476 ms (27.21%), Space: 13.9 MB (69.49%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -0,0 +1,25 @@
+class Solution:
+    def distributeCookies(self, cookies: List[int], k: int) -> int:
+        fair = [0 for _ in range(k)]
+        
+        def backT(i, child):
+            
+            if i >= len(cookies):
+                return max(child)
+            
+            noCh = child.count(0)
+            rem = len(cookies) - i
+            if noCh > rem:
+                return float("inf")
+            
+            temp = float("inf")
+            for j in range(k):
+                child[j] += cookies[i]
+                temp = min(temp, backT(i+1, child))
+                child[j] -= cookies[i]
+            return temp
+                    
+                
+        return backT(0, fair)
+        
+                
