1aa8f24612637358a305f87d393b68d4d03a3f2b
leulabay1 <108995687+leulabay1@users.noreply.github.com>
2023-03-09 06:53:45 +0300

    Time: 25 ms (97.12%), Space: 13.9 MB (15.17%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        def find_max(node):
+            if not node : return 0 
+            left = 1 + find_max(node.left)
+            right = 1 + find_max(node.right)
+
+            return max(left,right)
+        
+        depth = find_max(root)
+        ans = [[] for _ in range(depth)]
+        
+        def levelTraversing(p, level):
+            if p == None:
+                return 0
+            else:
+                ans[level].append(p.val)
+                if p.left != None:
+                    levelTraversing(p.left, level + 1)
+                if p.right != None:
+                    levelTraversing(p.right, level + 1)
+            
+            return 0
+        levelTraversing(root, 0)
+        
+        ans2 = []
+        for i in ans:
+            ans2.append(i[-1])
+        return ans2
