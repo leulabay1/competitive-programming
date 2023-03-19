294e9ac8d15a0dc100c4bc90a2c6cb6369adc657
leulabay1 <108995687+leulabay1@users.noreply.github.com>
2023-03-19 10:13:41 +0300

    Time: 28 ms (94.40%), Space: 13.8 MB (79.76%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -3,14 +3,12 @@ class Solution:
         ans = strs[0]
         for i in strs[1:]:
             temp = ""
-            l = 0
             n = min(len(i),len(ans))
-            while l < n:
+            for l in range(n):
                 if ans[l] == i[l]:
                     temp += ans[l]
                 else:
                     break
-                l += 1
             ans = temp
             if temp == "":
                 break
