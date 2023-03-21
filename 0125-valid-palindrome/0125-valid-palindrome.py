09198f7cbd9246b153972c64221c10f830b65ff1
leulabay1 <108995687+leulabay1@users.noreply.github.com>
2023-03-22 00:45:18 +0300

    Time: 49 ms (60.52%), Space: 14.6 MB (95.76%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -5,7 +5,6 @@ class Solution:
             if i.isalpha() or i.isdigit():
                 s += i.lower()
         
-        print(s)
         for i in range(len(s)//2):
             if s[i] != s[len(s)-i-1]:
                 return False
