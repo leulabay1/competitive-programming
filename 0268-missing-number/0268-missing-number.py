e15c7d284aae502b54bc4a06f00d49cd4ee337df
leulabay1 <108995687+leulabay1@users.noreply.github.com>
2023-03-29 19:21:19 +0300

    Time: 149 ms (31.79%), Space: 15.5 MB (14.39%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        tempDic = Counter(nums)
+        minVal = min(nums)
+        maxVal = max(nums)
+        if minVal != 0:
+            return 0
+        for i in range(minVal, maxVal+1):
+            if i not in tempDic:
+                return i
+        else:
+            return maxVal + 1
