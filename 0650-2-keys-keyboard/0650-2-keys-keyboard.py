2908c9cbb605837bcacda922ef70b5db1497f668
leulabay1 <108995687+leulabay1@users.noreply.github.com>
2023-04-04 18:38:30 +0300

    Time: 40 ms (61.73%), Space: 14 MB (35.39%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minSteps(self, n: int) -> int:
+        if n == 1:
+            return 0
+        for i in range(2,n):
+            if n%i == 0:
+                return self.minSteps(n//i) + 1 + (i - 1)
+        else:
+            return n
+        
