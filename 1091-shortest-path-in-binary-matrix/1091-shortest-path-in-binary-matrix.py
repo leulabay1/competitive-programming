3f7ca84cee1083d6b99433d74d9af80e3a2a626e
leulabay1 <108995687+leulabay1@users.noreply.github.com>
2023-04-27 18:37:16 +0300

    Time: 1400 ms (5.05%), Space: 19.4 MB (6.43%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -0,0 +1,37 @@
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        def bound(row, col):
+            if 0 <= row < n and 0 <= col < m:
+                return True
+            else:
+                return False
+        n, m = len(grid), len(grid[0])
+        direction = [(1,1),(0,1),(1,0),(1,-1),(-1,1),(-1,0),(0,-1),(-1,-1)]
+        
+        if grid[0][0]:
+            return -1
+        
+        visited = set()
+        que = deque([(0,0)])
+        parent = {(0,0): None}
+        
+        while que:
+            ro, co = que.popleft()
+            visited.add((ro,co))
+            
+            if (ro, co) == (n-1, m-1):
+                path = []
+                node = (ro, co)
+                while node:
+                    path.append(node)
+                    node = parent[node]
+                return len(path)
+            
+            for row, col in direction:
+                newrow = row + ro
+                newcol = col + co
+                if bound(newrow, newcol) and not grid[newrow][newcol] and (newrow,newcol) not in visited and (newrow,newcol) not in que:
+                    parent[(newrow, newcol)] = (ro, co)
+                    que.append((newrow, newcol))
+        return -1
+            
