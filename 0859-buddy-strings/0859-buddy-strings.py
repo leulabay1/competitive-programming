36630d55d787eb3283bd83a961a77c5f0a8f0587
leulabay1 <108995687+leulabay1@users.noreply.github.com>
2023-07-04 10:57:57 +0300

    Time: 52 ms (43.03%), Space: 16.6 MB (27.98%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.py
+++ 0859-buddy-strings/0859-buddy-strings.py
@@ -1,21 +1,19 @@
 class Solution:
     def buddyStrings(self, s: str, goal: str) -> bool:
         n = len(s)
-        if n != len(goal) or n < 2:
+        m = len(goal)
+        if n != m or n < 2:
             return False
+        if s == goal:
+            return len(s) - len(set(s)) >= 1
         dif = list()
         for i in range(n):
             if goal[i] != s[i]:
                 dif.append(i)
+                if len(dif) > 2:
+                    return False
                 
         if len(dif) == 2 and goal[dif[0]] == s[dif[1]] and goal[dif[1]] == s[dif[0]]:
             return True
-        elif len(dif) == 0:
-            freq = dict(Counter(s))
-            for val in freq.values():
-                if val > 1:
-                    return True
-            else:
-                return False
         else:
             return False
