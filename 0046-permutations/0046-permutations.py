2ff80e59ac70466db4af28e21d6e3afc217ab32e
leulabay1 <108995687+leulabay1@users.noreply.github.com>
2023-07-04 13:38:59 +0300

    Time: 56 ms (56.75%), Space: 16.6 MB (12.09%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -2,17 +2,18 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         ans = []
         n = len(nums)
-        def Tracker(arr, visited, i):
+        def backT(arr, check):
             nonlocal n
             if len(arr) == n:
                 ans.append(arr.copy())
                 return
-            for j in range(n):
-                if j not in visited:
-                    visited.add(j)
-                    arr.append(nums[j])
-                    Tracker(arr, visited, j)
+            for i in range(n):
+                if i not in check:
+                    check.add(i)
+                    arr.append(nums[i])
+                    backT(arr, check)
+                    check.remove(i)
                     arr.pop()
-                    visited.remove(j)
-        Tracker([], set(), -1)
+            return
+        backT([], set())
         return ans
