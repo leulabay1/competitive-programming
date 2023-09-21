d07adfd1abc9259a7cb2aa347389b08bbe8eae6e
Leul Abay <leulabay1@gmail.com>
2023-09-21 14:51:46 +0300

    Time: 523 ms (96.46%), Space: 34.4 MB (10.01%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,22 +1,22 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        def palindrom_checker(sub):
+        def p(sub):
             for i in range(len(sub)//2):
                 if sub[i] != sub[len(sub) - i - 1]:
                     return False
             return True
+        n = len(s)
+        dp = [[] for i in range(n + 1)]
+        dp[0] = [[""]]
+        
+        for i in range(1, n + 1):
+            for j in range(i):
+                if p(s[j:i]):
+                    if j == 0:
+                        dp[i].append([s[j:i]])
+                    elif dp[j]:
+                        for value in dp[j]:
+                            dp[i].append(value + [s[j:i]])
             
-        ans = []
         
-        def dp(i, arr):
-            if i >= len(s):
-                ans.append(arr.copy())
-                return
-            for idx in range(i + 1, len(s) + 1):
-                if palindrom_checker(s[i:idx]):
-                    arr.append(s[i:idx])
-                    dp(idx, arr)
-                    arr.pop()
-        
-        dp(0, [])
-        return ans
+        return dp[-1]
