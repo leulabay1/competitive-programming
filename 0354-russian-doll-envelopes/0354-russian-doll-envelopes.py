ac144ae100ad5f16f0443224d47f08610f443d3e
Leul Abay <leulabay1@gmail.com>
2023-09-30 21:34:20 +0300

    Time: 1018 ms (54.15%), Space: 64.1 MB (44.71%) - LeetHub

---
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+        envelopes.sort(key=lambda key:(key[0], -key[1]))
+        tails = []
+        for i in range(0, len(envelopes)):
+            idx = bisect.bisect_right(tails, envelopes[i][1])
+            if idx - 1 >= 0 and tails[idx - 1] == envelopes[i][1]:
+                continue
+            if idx == len(tails):
+                tails.append(envelopes[i][1])
+            else:
+                tails[idx] = envelopes[i][1]
+        return len(tails)
