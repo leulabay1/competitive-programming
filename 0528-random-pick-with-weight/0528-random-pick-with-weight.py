3fbd66f435a2b5ff61752bf92372faccd99ee800
Leul Abay <leulabay1@gmail.com>
2023-09-30 21:12:24 +0300

    Time: 170 ms (83.06%), Space: 37.6 MB (5.13%) - LeetHub

---
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -0,0 +1,19 @@
+class Solution:
+
+    def __init__(self, w: List[int]):
+        self.array = []
+        self.SUM = sum(w)
+        self.LENGTH = len(w)
+        self.PERCENT = 100
+        
+        for idx, weight in enumerate(w):
+            frequency = math.ceil(weight * self.LENGTH * self.PERCENT / self.SUM)
+            self.array += [idx] * frequency
+        
+    def pickIndex(self) -> int:
+        return self.array[random.randrange(len(self.array))]
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
