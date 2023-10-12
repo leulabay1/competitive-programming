3c5f4bda44c1207223c9c31a4915f99b80873abc
Leul Abay <leulabay1@gmail.com>
2023-10-12 18:28:37 +0300

    Time: 48 ms (15.51%), Space: 16.3 MB (67.42%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -1,9 +1,9 @@
 class Solution:
     def countOrders(self, n: int) -> int:
         
-        temp = 1
+        count = 1
         
         for i in range(0, n):
-            temp = temp * ((2*i + 2)*(2*i + 1)//2)
-            temp %= (10**9 + 7)
-        return temp
+            count = count * ((i + 1) * (2*i + 1))
+            count %= (10**9 + 7)
+        return count
